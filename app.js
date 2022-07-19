5d04b1eb1602366a601ee96a52f1096691b23d76
Mati_ toloo <matolox88@gmail.com>
2022-07-19 16:13:31 -0300

    subiendo

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const { resolve } = require('path')
+const path = require('path')
+const app = express()
+const publicPath = path.resolve(__dirname, './public')
+const port = process.env.PORT || 3000
+app.use (express.static(publicPath))
+
+app.listen(3000, () => console.log('Levantando un servidor con express'))
+
+app.get('/home', function(req, res){ res.sendFile(resolve(__dirname, 'views/index.html'))
+})
+
+app.get('/register', function(req, res){ res.sendFile(resolve(__dirname, 'views/register.html'))
+})
+
+app.get('/login', function(req, res){ res.sendFile(resolve(__dirname, 'views/login.html'))
+})
